a0c6afac5f0639aceeb6492a6ada29e6104228ad
Vinicius Michelin da Silva <vinnimichelin@gmail.com>
2024-03-06 14:49:04 -0300

    Primeiro Commit - Todos os arquivos

---
+++ index.js
@@ -0,0 +1,13 @@
+const colors = [ 'Red', 'Blue', 'Orange', 'Grey', 'Brown', 'Yellow', 'Purple', 'Green']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click',function(){
+    const randomColor = getRandomColor()
+    document.body.style.background = colors[randomColor]
+    color.textContent = colors[randomColor]
+})
+
+function getRandomColor(){
+    return Math.floor(Math.random() * colors.length)
+}
